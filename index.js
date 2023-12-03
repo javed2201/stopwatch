c3ec0ae8f9325bf971aeee5f3d64ec1b9e5d880e
javedShafiullah <javed.bd1988@gmail.com>
2023-12-03 13:31:07 +0600

    this project by chatgpt added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var viewHour =  document.getElementById("hours")
 
 function startCount() {
   // check if an interval has already been set up
-  if (!nIntervId) {
+  if (!nIntervId) { 								// চ্যাটজিপিটিতে এই চেকটা নাই
     nIntervId = setInterval(updateTime, 10);
 }
 }
@@ -35,18 +35,18 @@ function updateTime() {
 				// if(hour==3){ // if want day
 				// 	hour = 0
 				// }
-				hour<10 ? viewHour.innerHTML = `0${hour}` : viewHour.innerHTML = hour
+				hour<10 ? viewHour.innerHTML = `0${hour}` : viewHour.innerHTML = hour 					// চ্যাটজিপিটির ফরম্যাট আরো সুন্দর
 			}
-			minute<10 ? viewMinute.innerHTML = `0${minute}` : viewMinute.innerHTML = minute
+			minute<10 ? viewMinute.innerHTML = `0${minute}` : viewMinute.innerHTML = minute 			// চ্যাটজিপিটির ফরম্যাট আরো সুন্দর
 		}
-		second<10 ? viewSecond.innerHTML = `0${second}` : viewSecond.innerHTML = second
+		second<10 ? viewSecond.innerHTML = `0${second}` : viewSecond.innerHTML = second 				// চ্যাটজিপিটির ফরম্যাট আরো সুন্দর
 	}
-	miliSecond<10 ? viewMiliSecond.innerHTML = `0${miliSecond}` : viewMiliSecond.innerHTML = miliSecond
+	miliSecond<10 ? viewMiliSecond.innerHTML = `0${miliSecond}` : viewMiliSecond.innerHTML = miliSecond // চ্যাটজিপিটির ফরম্যাট আরো সুন্দর
 }
 
 function pauseTime() {
   clearInterval(nIntervId);
-  nIntervId = null;
+  nIntervId = null;							 // চ্যাটজিপিটির তুলনায় অতিরিক্ত
 }
 
 function stopTime() {
@@ -55,12 +55,12 @@ function stopTime() {
   second = 0
   minute = 0
   hour = 0
-  viewMiliSecond.innerHTML = `00`
-  viewSecond.innerHTML = `00`
-  viewMinute.innerHTML = `00`
-  viewHour.innerHTML = `00`
+  viewMiliSecond.innerHTML = `00` 			// চ্যাটজিপিটির তুলনায় অতিরিক্ত
+  viewSecond.innerHTML = `00` 				// চ্যাটজিপিটির তুলনায় অতিরিক্ত
+  viewMinute.innerHTML = `00` 				// চ্যাটজিপিটির তুলনায় অতিরিক্ত
+  viewHour.innerHTML = `00` 				// চ্যাটজিপিটির তুলনায় অতিরিক্ত
   // release our intervalID from the variable
-  nIntervId = null;
+  nIntervId = null; 						// চ্যাটজিপিটির তুলনায় অতিরিক্ত
 }
 
 document.getElementById("start").addEventListener("click", startCount);
@@ -68,3 +68,48 @@ document.getElementById("stop").addEventListener("click", stopTime);
 document.getElementById("pause").addEventListener("click", pauseTime);
 
 
+
+// much cleac code by chatGpt
+
+// let timer;
+// let seconds = 0, minutes = 0, hours = 0;
+
+// function startTimer() {
+// 	timer = setInterval(updateTimer, 1000);
+// }
+
+// function updateTimer() {
+// 	seconds++;
+// 	if (seconds === 60) {
+// 		seconds = 0;
+// 		minutes++;
+// 		if (minutes === 60) {
+// 			minutes = 0;
+// 			hours++;
+// 		}
+// 	}
+
+// 	document.getElementById('hours').innerText = formatTime(hours);
+// 	document.getElementById('minutes').innerText = formatTime(minutes);
+// 	document.getElementById('seconds').innerText = formatTime(seconds);
+// }
+
+// function formatTime(time) {
+// 	return time < 10 ? `0${time}` : time;
+// }
+
+// function pauseTimer() {
+// 	clearInterval(timer);
+// }
+
+// function stopTimer() {
+// 	clearInterval(timer);
+// 	seconds = 0;
+// 	minutes = 0;
+// 	hours = 0;
+// 	updateTimer();
+// }
+
+// document.getElementById('start').addEventListener('click', startTimer);
+// document.getElementById('stop').addEventListener('click', stopTimer);
+// document.getElementById('pause').addEventListener('click', pauseTimer);
